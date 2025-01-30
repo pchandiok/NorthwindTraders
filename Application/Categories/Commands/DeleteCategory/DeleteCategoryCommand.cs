ee41ce694ecde26d70760040b78a1045a85a8805
Puneet Chandiok <pchandiok@outlook.com>
2025-01-30 19:06:18 +0530

    Made some changes

--- Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
+++ Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -1,5 +1,13 @@
+using MediatR;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Models.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Application.Categories.Commands.DeleteCategory;
 
-public class DeleteCategoryCommand
+public class DeleteCategoryCommand : IRequest
 {
+    public int Id { get; set; }
 }
